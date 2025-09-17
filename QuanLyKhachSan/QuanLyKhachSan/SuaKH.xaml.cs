e1e9e8c07faea05213e068c2d513973954705173
LowMhandsome <manh31072004@gmail.com>
2025-09-17 23:48:16 +0700

    update ChiTietPhong

--- QuanLyKhachSan/QuanLyKhachSan/SuaKH.xaml.cs
+++ QuanLyKhachSan/QuanLyKhachSan/SuaKH.xaml.cs
@@ -23,5 +23,12 @@ namespace QuanLyKhachSan
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
     }
 }
--- QuanLyKhachSan/QuanLyKhachSan/ThemKHxaml.xaml.cs
+++ QuanLyKhachSan/QuanLyKhachSan/ThemKHxaml.xaml.cs
@@ -23,5 +23,12 @@ namespace QuanLyKhachSan
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
     }
 }
